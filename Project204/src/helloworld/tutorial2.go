c2b9a1b12be047fc8e535a6d0fd3655404aa6009
Fuhrer <2962928213@qq.com>
2020-12-29 13:42:38 +0800

    update

--- Project204/src/helloworld/tutorial2.go
+++ Project204/src/helloworld/tutorial2.go
@@ -1,6 +1,13 @@
 package helloworld
 
 /*练习一下go test*/
-func IsEven(x int)bool{
-	return true
+func IsEven(x int) bool {
+	if x == 1 {
+		return false
+	} else if x%2 == 0 {
+		return true
+	} else if x%2 != 0 {
+		return false
+	}
+	return false
 }
--- Project204/src/helloworld/tutorial2_test.go
+++ Project204/src/helloworld/tutorial2_test.go
@@ -2,11 +2,16 @@ package helloworld
 
 import "testing"
 
-func TestIsEven(t *testing.T){
-	for i:=0;i<10;i++{
-		if tmp:=IsEven(i);tmp!=(i%2==0){
-			t.Errorf("Iseven(%d)=%v,exected %v",i,tmp,i%2==0)
+func TestIsEven(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		if tmp := IsEven(i); tmp != (i%2 == 0) {
+			t.Errorf("Iseven(%d)=%v,exected %v", i, tmp, i%2 == 0)
 		}
 	}
 }
 
+func BenchmarkIsEven(t *testing.B) {
+	for i := 0; i < 10; i++ {
+		IsEven(i)
+	}
+}
